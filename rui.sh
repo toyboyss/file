4656967405b96ee5bff04ffd78398e9327024247
pphub2019 <51102244+pphub2019@users.noreply.github.com>
2019-10-25 21:53:36 +0800

    Create rui.sh

---
+++ rui.sh
@@ -0,0 +1,11 @@
+
+
+wget http://security-cdn.debian.org/pool/updates/main/l/linux/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb && dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
+
+或者
+
+wget https://debian.sipwise.com/debian-security/pool/main/l/linux/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb && dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
+
+或者
+
+
