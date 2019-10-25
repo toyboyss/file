ffb072b38806d90c7a812e33022a77189da541af
pphub2019 <51102244+pphub2019@users.noreply.github.com>
2019-10-25 22:54:02 +0800

    Update rui.sh

--- rui.sh
+++ rui.sh
@@ -9,6 +9,6 @@ wget https://debian.sipwise.com/debian-security/pool/main/l/linux/linux-image-4.
 或者
 
 
-wget "https://github.com/pphub2019/file/raw/master/linux-image-4.9.0-4-amd64_4.9.65-3%2Bdeb9u1_amd64.deb" -O linux-image-4.9.0-4-amd64_4.9.65-3%2Bdeb9u1_amd64.deb && dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
+wget "https://github.com/pphub2019/file/raw/master/linux-image-4.9.0-4-amd64_4.9.65-3%2Bdeb9u1_amd64.deb" -O  linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb && dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
 
 
