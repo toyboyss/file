ba086b03ef3f305df48b33801e536395ff44f2e6
pphub2019 <51102244+pphub2019@users.noreply.github.com>
2019-10-26 09:21:32 +0800

    Create sspanel.sh

---
+++ sspanel.sh
@@ -0,0 +1,2 @@
+wget -N --no-check-certificate https://raw.githubusercontent.com/pphub2019/sspanel-v3-mod-uim-bt/master/sspanel-v3-mod-uim-bt.sh && chmod +x sspanel-v3-mod-uim-bt.sh && bash sspanel-v3-mod-uim-bt.sh
+
