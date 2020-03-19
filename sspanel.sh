ea207369435d26b3db2d0c99327d70486c448693
toyboyss <57551791+toyboyss@users.noreply.github.com>
2020-03-19 21:37:18 +0800

    Delete sspanel.sh

--- sspanel.sh
+++
@@ -1,2 +0,0 @@
-wget -N --no-check-certificate https://raw.githubusercontent.com/pphub2019/sspanel-v3-mod-uim-bt/master/sspanel-v3-mod-uim-bt.sh && chmod +x sspanel-v3-mod-uim-bt.sh && bash sspanel-v3-mod-uim-bt.sh
-
